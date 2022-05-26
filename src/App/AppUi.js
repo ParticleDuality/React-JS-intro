b189d76f37ba1a7b4eaf42cbbd4122defe2cbc61
Santiago Pulido <david_santiago@hotmail.es>
2022-05-26 15:21:24 -0500

    chore (App): Se empieza a usar context y estado global

--- src/App/AppUi.js
+++ src/App/AppUi.js
@@ -1,35 +1,36 @@
 import React from 'react'
+import { Context } from '../Context'
 import { Header } from "../Header"
 import { SearchBox } from "../SearchBox"
 import { List } from "../List"
 import { Item } from "../Item"
 import { CreateButton } from "../CreateButton"
 
-export function AppUi({
-  totalTodos,
-  completedTodos,
-  searchValue,
-  setSearchValue,
-  searchedTodos,
-  changeTodoStatus,
-  deleteTodo
-}) {
+export function AppUi() {
   return (
     <>
-      <Header total={totalTodos} completed={completedTodos} />
+      <Header />
 
-      <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      <SearchBox />
 
-      <List>
-        {searchedTodos.map((todo, index) => (
-          <Item
-            key={index}
-            {...todo}
-            onChangeStatus={() => changeTodoStatus(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </List>
+      <Context.Consumer>
+        {({ error, loading, changeTodoStatus, deleteTodo, searchedTodos }) => (
+          <List>
+            {error && <p>Ha ocurrido un error</p>}
+            {loading && <p>Cargando...</p>}
+            {!loading && !searchedTodos.length && <p>Crea tu primer TODO</p>}
+
+            {searchedTodos.map((todo, index) => (
+              <Item
+                key={index}
+                {...todo}
+                onChangeStatus={() => changeTodoStatus(todo.text)}
+                onDelete={() => deleteTodo(todo.text)}
+              />
+            ))}
+          </List>
+        )}
+      </Context.Consumer>
 
       <CreateButton />
     </>
--- src/App/Index.js
+++ src/App/Index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { AppUi } from "./AppUi"
-import { useLocalStorage } from "../Hooks/useLocalStorage"
+import { Provider } from "../Context"
 
 // const DEFAULT_TODOS = [
 //   { text: "Estudiar", completed: true },
@@ -8,52 +8,10 @@ import { useLocalStorage } from "../Hooks/useLocalStorage"
 //   { text: "Leer", completed: false },
 // ]
 export function App() {
-  const [todos, saveTodos] = useLocalStorage("TODOS_V1", [])
-
-  const [searchValue, setSearchValue] = React.useState('')
-
-  const completedTodos = todos.filter(todo => todo.completed).length
-  const totalTodos = todos.length
-
-  let searchedTodos = todos
-
-  if (searchValue.length > 0) {
-    searchedTodos = todos.filter(todo => {
-      const todoText = todo.text.toLowerCase()
-      const searchText = searchValue.toLowerCase()
-
-      return todoText.includes(searchText)
-    })
-  }
-
-  const changeTodoStatus = (name) => {
-    const todoIndex = todos.findIndex((todo) => todo.text === name)
-    const newTodos = [...todos]
-    
-    newTodos[todoIndex].completed === true
-      ? newTodos[todoIndex].completed = false
-      : newTodos[todoIndex].completed = true 
-    saveTodos(newTodos)
-  }
-
-  const deleteTodo = (name) => {
-    const todoIndex = todos.findIndex((todo) => todo.text === name)
-    const newTodos = [...todos]
-
-    newTodos.splice(todoIndex, 1)
-    saveTodos(newTodos)
-  }
-
   return (
-    <AppUi
-      totalTodos={totalTodos}
-      completedTodos={completedTodos}
-      searchValue={searchValue}
-      setSearchValue={setSearchValue}
-      searchedTodos={searchedTodos}
-      changeTodoStatus={changeTodoStatus}
-      deleteTodo={deleteTodo}
-    />
+    <Provider>
+      <AppUi />
+    </Provider>
   )
 }
 
