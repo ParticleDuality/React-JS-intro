488152225a5822a140e8d81b62059dea0958d9a2
Santiago Pulido <david_santiago@hotmail.es>
2022-05-31 01:07:42 -0500

    Updates

--- src/App/Error.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './Error.css'
-
-export function Error({ error }) {
-  return (
-    <div className='error-label'>Oops... No pudimos cargar tus TODO, inténtalo más tarde.</div>
-  )
-}
--- src/List/Index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './List.css'
-
-export function List(props) {
-  return (
-    <ul>{ props.children }</ul>
-  )
-}
